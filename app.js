29516dfc2dfa98b8c383a2008d91b34632482979
malditoprotozoo <virodriguez.fe@gmail.com>
2018-01-25 10:57:29 -0300

    Functions fixed based on Google's style guide for JS

--- app.js
+++ app.js
@@ -1,63 +1,69 @@
-/*This will check if a credit card numbers is valid, according
+/* This will check if a credit card numbers is valid, according
 to the Luhn Algorithm */
+
 function isValidCard(numbers) {
-
-    do {
-        numbers = prompt("Por favor, ingrese el número de su tarjeta de crédito, sin espacios.");
-        // If the person ignore this warning, the number will be considered invalid
-        if (numbers !== "" && numbers !== " ") {
-            var arr = numbers.split(""); //The input will be a string, so this converts it into an array
-        } else {
-            alert("Ingrese un número válido ¬¬");
-        }
-    } while (numbers === "" || numbers === " ");
-
-
-    var arrReverse = arr.reverse(); //Will contain the array in reverse
-    var arrOfNumbers = []; /*So far, the numbers are on an array, but each one is typeof string. 
-    This will contain each digit typeof number.*/
-    var sumOdds = 0; //It'll sum numbers at odd positions
-    var even = []; //It'll contain numbers at even positions
-    var evenPerTwo = []; //It'll contain numbers at even positions multiplied per two
-    var singlefy = []; //It'll contain numbers smaller than nine.
-    var evenFinal = 0; //It'll will sum numbers at even positions
-    var check = ""; //It'll contain the final result.
-
-    for (var i = 0; i < arrReverse.length; i++) {
-        arrOfNumbers.push(parseInt(arrReverse[i]));
-    }
-
-
-    for (var a = 0; a < arrOfNumbers.length; a++) {
-        if (a % 2 === 0) { //Positions in JS are "reversed", so the even positions are the odd positions.
-            sumOdds += arrOfNumbers[a];
-        } else if (a % 2 !== 0) {
-            even.push(arrOfNumbers[a]);
-        }
-    }
-
-    for (var b = 0; b < even.length; b++) {
-        evenPerTwo.push(even[b] * 2);
-    }
-
-    for (var c = 0; c < evenPerTwo.length; c++) {
-        if (evenPerTwo[c] > 9) {
-            singlefy.push(evenPerTwo[c] - 9);
-        } else {
-            singlefy.push(evenPerTwo[c]);
-        }
-    }
-
-    for (var d = 0; d < singlefy.length; d++) {
-        evenFinal = evenFinal + singlefy[d];
-    }
-
-    if ((evenFinal + sumOdds) % 10 === 0) {
-        check = "Yay! Tu tarjeta es válida";
+  do {
+    numbers = prompt('Por favor, ingrese el número de su tarjeta de crédito, sin espacios.');
+    // If the person ignore this warning, the number will be considered invalid
+    if (numbers !== '' && numbers !== ' ') {
+      // The input will be a string, so this converts it into an array
+      var arr = numbers.split('');
     } else {
-        check = "Lo sentimos, tu tarjeta es inválida :(";
+      alert('Ingrese un número válido ¬¬');
     }
+  } while (numbers === '' || numbers === ' ');
+  // Will contain the array in reverse
+  var arrReverse = arr.reverse();
+  var arrOfNumbers = [];
+  /* So far, the numbers are on an array, but each one is typeof string.
+  This will contain each digit typeof number. */
+  // It'll sum numbers at odd positions
+  var sumOdds = 0;
+  // It'll contain numbers at even positions
+  var even = [];
+  // It'll contain numbers at even positions multiplied per two
+  var evenPerTwo = [];
+  // It'll contain numbers smaller than nine.
+  var singlefy = [];
+  // It'll will sum numbers at even positions
+  var evenFinal = 0;
+  // It'll contain the final result.
+  var check = '';
 
-    return alert(check);
+  for (var i = 0; i < arrReverse.length; i++) {
+    arrOfNumbers.push(parseInt(arrReverse[i]));
+  };
 
-}
+  for (var num = 0; num < arrOfNumbers.length; num++) {
+    // Positions in JS are 'reversed', so the even positions are the odd positions.
+    if (num % 2 === 0) {
+      sumOdds += arrOfNumbers[a];
+    } else if (num % 2 !== 0) {
+      even.push(arrOfNumbers[num]);
+    }
+  };
+
+  for (var index = 0; index < even.length; index++) {
+    evenPerTwo.push(even[index] * 2);
+  };
+
+  for (var inter = 0; inter < evenPerTwo.length; inter++) {
+    if (evenPerTwo[inter] > 9) {
+      singlefy.push(evenPerTwo[inter] - 9);
+    } else {
+      singlefy.push(evenPerTwo[inter]);
+    }
+  };
+
+  for (var digit = 0; digit < singlefy.length; digit++) {
+    evenFinal = evenFinal + singlefy[digit];
+  };
+
+  if ((evenFinal + sumOdds) % 10 === 0) {
+    check = 'Yay! Tu tarjeta es válida';
+  } else {
+    check = 'Lo sentimos, tu tarjeta es inválida :(';
+  };
+
+  return alert(check);
+};
